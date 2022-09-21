8d2dfedf9548d861ae5f05a3e137532bee49efa2
perdanaph <danangputro@gmail.com>
2022-09-21 17:20:18 +0700

    create: middleware auth

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+/* eslint-disable dot-notation */
+const { verify } = require('../helpers/jwtHelper')
+
+exports.userVerify = (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization']
+    if (!authHeader) {
+      return res.status(403).json({ message: 'before request data, please login first' })
+    }
+    const token = authHeader.split(' ')[1]
+    const { id, name } = verify(token)
+    req.user = { id, name }
+    next()
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
--- src/routes/note.js
+++ src/routes/note.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const noteController = require('../controllers/noteController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
-router.get('/notes', noteController.getAllNotes)
+router.get('/notes', authMiddleware.userVerify, noteController.getAllNotes)
 
 module.exports = router
