b792cf3ccd7bca44ed5b8263755e7477c38b8215
perdanaph <danangputro@gmail.com>
2022-09-21 14:33:10 +0700

    create: initial project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hola')
+})
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`)
+})
