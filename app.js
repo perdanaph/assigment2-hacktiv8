f5918ba54fd1012949ee50d726857da2668ed55a
perdanaph <danangputro@gmail.com>
2022-09-21 16:22:05 +0700

    add: route user and config route in server file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
-
 const app = express()
+const route = require('./src/routes/index')
 const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/v1', route)
 
 app.get('/', (req, res) => {
   res.send('Hola')
